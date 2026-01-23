c8e4d42fc41862f1f240bdf39ad7ae38ea34142c
subba reddy arikatla <subbareddyarikatla8@gmail.com>
2026-01-23 12:59:00 +0530

    Add array DSA problems and update search & insert solution

--- serachandinsertproblem.py
+++ serachandinsertproblem.py
@@ -1,13 +1,20 @@
-def arraydemo(array,target):
-    if not target in array:
-        array.append(target)
-        return array
-    for i in range(len(array)):
-        if array[i]==target:
+# def arraydemo(array,target):
+#     for i in range(len(array)):
+#         if array[i]==target:
+#             return i
+#         elif array[i]>target:
+#             array.insert(i,target)
+#             return i
+#         elif(target>array[i]):
+#             array.append(target)
+#             return len(array)-1
+        
+
+def searchInsert(arr,k):
+    for i in range(len(arr)):
+        if arr[i]>=k:
             return i
-        elif array[i]>target:
-            array.insert(i,target)
-            return i
-        elif(target>array[i]):
-            array.append(target)
-            return len(array)-1
+    return len(arr)
+arr = [1, 3, 5, 6]  
+k = 5  
+print(searchInsert(arr, k))
