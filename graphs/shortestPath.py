20110300821787535c7842479ec057cedb97f927
subba reddy arikatla <subbareddyarikatla8@gmail.com>
2026-03-27 11:41:05 +0530

    ne change shortest path

--- graphs/shortestPath.py
+++ graphs/shortestPath.py
@@ -33,3 +33,47 @@ if __name__=='__main__':
         graph[edge[0]].append(edge[1])
         graph[edge[1]].append(edge[0])
         print_shortest_distance(graph,s,d,v)
+
+def print_shortest_distance(graph, S, D, V):
+    par=[-1]*V
+    dist=[float('inf')]*V
+    bfs(graph,s,par,dist)
+    if dist[D]==float('inf'):
+        print("source and desitination are connect")
+        return
+
+if __name__=='__main__':
+    v=8
+    s,d=2,6
+    edges = [
+        [0, 1], [1, 2], [0, 3], [3, 4],
+        [4, 7], [3, 7], [6, 7], [4, 5],
+        [4, 6], [5, 6]
+    ]
+    graph=[[] for _ in range(v)]
+    for edge in edges:
+        graph[edge[0]].append(edge[1])
+        graph[edge[1]].append(edge[0])
+        print_shortest_distance(graph,s,d,v)
+
+def print_shortest_distance(graph, S, D, V):
+    par=[-1]*V
+    dist=[float('inf')]*V
+    bfs(graph,s,par,dist)
+    if dist[D]==float('inf'):
+        print("source and desitination are connect")
+        return
+
+if __name__=='__main__':
+    v=8
+    s,d=2,6
+    edges = [
+        [0, 1], [1, 2], [0, 3], [3, 4],
+        [4, 7], [3, 7], [6, 7], [4, 5],
+        [4, 6], [5, 6]
+    ]
+    graph=[[] for _ in range(v)]
+    for edge in edges:
+        graph[edge[0]].append(edge[1])
+        graph[edge[1]].append(edge[0])
+        print_shortest_distance(graph,s,d,v)
